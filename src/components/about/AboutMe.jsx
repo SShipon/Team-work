6e859f12bfd6a967354887d12f597e5e9c8d37e2
SShipon <nxshipon@gmail.com>
2024-10-13 12:59:23 -0700

    solved this error

--- src/components/about/AboutMe.jsx
+++ src/components/about/AboutMe.jsx
@@ -2,6 +2,7 @@ const AboutMe = () => {
   return (
     <div>
       We&apos;re Team
+      we re team hello world Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat qui sapiente amet est officia temporibus odit voluptatibus reiciendis accusamus repellat!
     </div>
   );
 }
